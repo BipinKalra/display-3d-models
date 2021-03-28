77e556ee53d547177cf6971483dd564098aa1aee
BipinKalra <bipinkalra@gmail.com>
2021-03-28 18:47:37 +0530

    server

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const express = require("express");
+const socketIO = require("socket.io");
+const http = require("http");
+const https = require("https");
+
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+var count = 0;
+
+var $ipsConnected = [];
+
+io.on("connection", function (socket) {
+  var $ipAddress = socket.request.connection.remoteAddress;
+
+  if (!$ipsConnected.hasOwnProperty($ipAddress)) {
+    $ipsConnected[$ipAddress] = 1;
+
+    count++;
+
+    socket.emit("counter", { count: count });
+  }
+
+  socket.on("disconnect", function () {
+    if ($ipsConnected.hasOwnProperty($ipAddress)) {
+      delete $ipsConnected[$ipAddress];
+
+      count--;
+
+      socket.emit("counter", { count: count });
+    }
+  });
+});
+
+app.use(express.static(publicPath));
+
+server.listen(3000, () => {
+  console.log("listening at 3000");
+});
